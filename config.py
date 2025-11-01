6fcc7b7bfaf9d18c0c089769b913680ae2f9cb36
murodov109 <akabekov77@gmail.com>
2025-11-01 12:52:44 +0500

    Add initial configuration settings in config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+import os
+
+BOT_TOKEN = "8435705646:AAEJULzHr7N2Z-lleYsoQKf36yVnHxd0iXU"
+
+ADMINS = [7617397626]
+
+MANDATORY_CHANNELS = [
+    "@jonli_obunachipro",
+    "@kerakli_xizmatlarn1"
+]
+
+CARD_NUMBER = "8600 1234 5678 9012"
+
+PRICE_PER_SUB = 100
+REF_BONUS = 300
+
+BONUS_MIN = 10
+BONUS_MAX = 100
+
+DB_PATH = "database/data.json"
+
+HOST = "0.0.0.0"
+PORT = 8080
