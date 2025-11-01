7c2609b4f86f7894544c93bf6871aa613d254edd
murodov109 <akabekov77@gmail.com>
2025-11-01 20:11:23 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,19 +2,17 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8435705646:AAEJULzHr7N2Z-lleYsoQKf36yVnHxd0iXU")
 BOT_USERNAME = os.getenv("BOT_USERNAME", "@jonli_obunachipro_bot")
+
 ADMINS = [7617397626]
+
 CARD_NUMBER = "5440810305608647"
 
-PRICE_PER_SUB = 0.15
-REF_BONUS = 1.0
-BONUS_MIN = 0.5
-BONUS_MAX = 2.0
+PRICE_PER_SUB = 100
+REF_BONUS = 200
+BONUS_MIN = 50
+BONUS_MAX = 150
 
 MANDATORY_CHANNELS = [
-    "https://t.me/jonli_obunachipro",
-    "https://t.me/test_kanal_1"
+    "@testkanal1",
+    "@testkanal2"
 ]
-
-DB_NAME = "database.db"
-ON_RENDER = os.getenv("ON_RENDER", "False").lower() == "true"
-TOKEN = BOT_TOKEN
