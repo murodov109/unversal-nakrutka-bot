e92b56cdfed4dc7443441d7d44f7ab2cf7aafe84
murodov109 <akabekov77@gmail.com>
2025-11-01 15:47:41 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,23 +1,11 @@
 import os
 
-TOKEN = "8435705646:AAEJULzHr7N2Z-lleYsoQKf36yVnHxd0iXU"
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8435705646:AAEJULzHr7N2Z-lleYsoQKf36yVnHxd0iXU")
+
+BOT_USERNAME = os.getenv("BOT_USERNAME", "@jonli_obunachipro_bot")
 
 ADMINS = [7617397626]
 
-MANDATORY_CHANNELS = [
-    "@jonli_obunachipro",
-    "@kerakli_xizmatlarn1"
-]
+CARD_NUMBER = os.getenv("CARD_NUMBER", "8600123456789012")
 
-CARD_NUMBER = "8600 1234 5678 9012"
-
-PRICE_PER_SUB = 100
-REF_BONUS = 300
-
-BONUS_MIN = 10
-BONUS_MAX = 100
-
-DB_PATH = "database/data.json"
-
-HOST = "0.0.0.0"
-PORT = 8080
+DB_PATH = os.getenv("DB_PATH", "data.db")
