b2007d992919dd52e14f1619a612aaeeafe2f881
murodov109 <akabekov77@gmail.com>
2025-11-01 16:44:48 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
 import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8435705646:AAEJULzHr7N2Z-lleYsoQKf36yVnHxd0iXU")
-
 BOT_USERNAME = os.getenv("BOT_USERNAME", "@jonli_obunachipro_bot")
-
 ADMINS = [7617397626]
-
-CARD_NUMBER = os.getenv("CARD_NUMBER", "8600123456789012")
-
-DB_PATH = os.getenv("DB_PATH", "data.db")
+CARD_NUMBER = "5440810305608647"
+DB_NAME = "database.db"
+ON_RENDER = os.getenv("ON_RENDER", "False").lower() == "true"
+TOKEN = BOT_TOKEN
